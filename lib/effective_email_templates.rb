a72d0bd85aef1efb198d353e806080d44de913f4
Matt Riemer <matthew@agilestyle.com>
2014-11-26 09:09:29 -0700

    authorized? method

--- lib/effective_email_templates.rb
+++ lib/effective_email_templates.rb
@@ -12,4 +12,11 @@ module EffectiveEmailTemplates
     yield self
   end
 
+  def self.authorized?(controller, action, resource)
+    if authorization_method.respond_to?(:call) || authorization_method.kind_of?(Symbol)
+      raise Effective::AccessDenied.new() unless (controller || self).instance_exec(controller, action, resource, &authorization_method)
+    end
+    true
+  end
+
 end
