14c39615602248e564be569553c8a4e40c2c7fd8
Nathan Feaver <nathan@agilestyle.com>
2014-12-01 15:24:34 -0700

    add basic improvements to datatable

--- app/models/effective/datatables/email_templates.rb
+++ app/models/effective/datatables/email_templates.rb
@@ -3,6 +3,10 @@ if defined?(EffectiveDatatables)
     module Datatables
       class EmailTemplates < Effective::Datatable
         table_column :id
+        table_column :slug
+        table_column :subject
+        table_column :from
+        table_column :actions, sortable: false, partial: "/admin/email_templates/actions"
 
         def collection
           Effective::EmailTemplate.all
@@ -12,3 +16,4 @@ if defined?(EffectiveDatatables)
     end
   end
 end
+
