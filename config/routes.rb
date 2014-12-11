bbc94a593321e9bb38fd329dc4c547ec9272975b
Nathan Feaver <nathan@agilestyle.com>
2014-12-11 10:25:06 -0700

    remove unused routes and add Bootstrap classes for 'New Email Template' link

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,9 @@ Rails.application.routes.draw do
 end
 
 EffectiveEmailTemplates::Engine.routes.draw do
-  scope :module => 'effective' do
-    resources :email_templates
-    # Gets or Matches or whatever here
-  end
-
   if defined?(EffectiveDatatables)
     namespace :admin do
-      resources :email_templates
+      resources :email_templates, only: [:index, :new, :create, :edit, :update]
     end
   end
-
 end
