ee054d1c08ba60a4ad4ccf0e549b44abba9abad3
Matt Riemer <matthew@agilestyle.com>
2015-07-24 15:30:07 -0600

    Upgrade effective datatables

--- app/models/effective/datatables/email_templates.rb
+++ app/models/effective/datatables/email_templates.rb
@@ -2,16 +2,18 @@ if defined?(EffectiveDatatables)
   module Effective
     module Datatables
       class EmailTemplates < Effective::Datatable
-        default_order :subject, :asc
+        datatable do
+          default_order :subject, :asc
 
-        table_column :id, visible: false
-        table_column :slug
-        table_column :subject
-        table_column :from
-        table_column :cc, sortable: false, visible: false, label: 'CC'
-        table_column :bcc, sortable: false, visible: false, label: 'BCC'
-        table_column :body, sortable: false, visible: false
-        table_column :actions, sortable: false, partial: '/admin/email_templates/actions'
+          table_column :id, visible: false
+          table_column :slug
+          table_column :subject
+          table_column :from
+          table_column :cc, sortable: false, visible: false, label: 'CC'
+          table_column :bcc, sortable: false, visible: false, label: 'BCC'
+          table_column :body, sortable: false, visible: false
+          table_column :actions, sortable: false, partial: '/admin/email_templates/actions'
+        end
 
         def collection
           Effective::EmailTemplate.all
--- effective_email_templates.gemspec
+++ effective_email_templates.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'factory_girl_rails'
   s.add_development_dependency "sqlite3"
-  s.add_development_dependency "effective_datatables", '0.3.17'
+  s.add_development_dependency "effective_datatables", '>= 2.0.0'
   s.add_development_dependency "pry"
   s.add_development_dependency "devise"
 end
