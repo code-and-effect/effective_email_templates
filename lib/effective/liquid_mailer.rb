f0d76f1d617741ce5c1f8fba79782b83b7f3d27c
Matt Riemer <matthew@codeandeffect.com>
2019-08-07 11:58:48 -0600

    Use ApplicationMailer if present

--- lib/effective/liquid_mailer.rb
+++ lib/effective/liquid_mailer.rb
@@ -1,5 +1,5 @@
 module Effective
-  class LiquidMailer < ::ActionMailer::Base
+  class LiquidMailer < (defined?(::ApplicationMailer) ? ::ApplicationMailer : ::ActionMailer::Base)
     def mail(headers = {}, &block)
       # this be dangerous and requires ruby 2.0+
       mail_method = caller_locations(1, 1)[0].label
