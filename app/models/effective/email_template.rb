f65c105e11b14ef61f8777b32579ccfc0ab05b63
Matt Riemer <matthew@codeandeffect.com>
2020-05-27 11:07:30 -0600

    update result

--- app/models/effective/email_template.rb
+++ app/models/effective/email_template.rb
@@ -51,15 +51,14 @@ module Effective
     def render(assigns = {})
       assigns = deep_stringify_assigns(assigns)
 
-      result={
+      result = {
         from: from,
+        cc: cc.presence,
+        bcc: bcc.presence,
         content_type: content_type,
         subject: template_subject.render(assigns),
         body: template_body.render(assigns)
-      }
-      result[:cc]= cc.presence if cc.presence
-      result[:bcc]= bcc.presence if bcc.presence
-      result
+      }.compact
     end
 
     def template_variables
