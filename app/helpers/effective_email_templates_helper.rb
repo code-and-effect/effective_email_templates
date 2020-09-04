de8dc2040882f36f6ae594725d2c826e494d7f89
Matt Riemer <matthew@codeandeffect.com>
2020-09-04 14:05:36 -0600

    Check for rendered message

--- app/helpers/effective_email_templates_helper.rb
+++ app/helpers/effective_email_templates_helper.rb
@@ -1,11 +1,19 @@
 module EffectiveEmailTemplatesHelper
 
-  def effective_email_review_fields(form, template_name)
+  # We are given a form to essentially send an email
+  def effective_email_review_fields(form, template_name, mail = nil)
+    raise('expected form.object to respond to email_review') unless form.object.respond_to?(:email_review)
+
     email_review = form.object.email_review
 
     unless email_review&.template_name == template_name.to_s
       email_template = Effective::EmailTemplate.where(template_name: template_name).first!
       email_review = Effective::EmailReview.build(email_template: email_template)
+    end
+
+    if mail.present?
+      raise('expected a mail object') unless mail.kind_of?(ActionMailer::MessageDelivery)
+      email_review.body ||= mail.message.body
     end
 
     render(partial: 'effective/email_reviews/fields', locals: { email_review: email_review, form: form })
