9a47f0ebd49c07ba6c2bd1f7fbf7a1e7e0837280
Matt Riemer <matthew@codeandeffect.com>
2021-11-15 16:23:35 -0700

    extract mailer into a concern

---
+++ app/mailers/concerns/effective_email_templates_mailer.rb
@@ -0,0 +1,35 @@
+# HasOneEmailReview
+# Allows any model to easily review an email template and make changes to the body
+
+module EffectiveEmailTemplatesMailer
+  extend ActiveSupport::Concern
+
+  def mail(headers = {}, &block)
+    email_template = Effective::EmailTemplate.where(template_name: action_name).first!
+    assigns = (@assigns || {})
+
+    # Parse assigns. Special keys for body and subject.
+    body = assigns.delete(:body) || headers[:body] || headers['body']
+    email_template.body = body if body.present?
+
+    subject = assigns.delete(:subject) || headers[:subject] || headers['subject']
+    email_template.subject = subject if subject.present?
+
+    # Add any _url helpers
+    assigns = route_url_assigns(email_template).merge(assigns)
+
+    # Render from the template, possibly with updated body
+    rendered = email_template.render(assigns)
+
+    super(rendered.merge(headers.except(:body, :subject, 'body', 'subject')))
+  end
+
+  private
+
+  def route_url_assigns(email_template)
+    email_template.template_variables.select { |name| name.ends_with?('_url') }.inject({}) do |h, name|
+      h[name] = public_send(name) if respond_to?(name); h
+    end
+  end
+
+end
--- app/mailers/effective/email_templates_mailer.rb
+++ app/mailers/effective/email_templates_mailer.rb
@@ -1,33 +1,5 @@
 module Effective
   class EmailTemplatesMailer < ::ActionMailer::Base
-
-    def mail(headers = {}, &block)
-      email_template = Effective::EmailTemplate.where(template_name: action_name).first!
-      assigns = (@assigns || {})
-
-      # Parse assigns. Special keys for body and subject.
-      body = assigns.delete(:body) || headers[:body] || headers['body']
-      email_template.body = body if body.present?
-
-      subject = assigns.delete(:subject) || headers[:subject] || headers['subject']
-      email_template.subject = subject if subject.present?
-
-      # Add any _url helpers
-      assigns = route_url_assigns(email_template).merge(assigns)
-
-      # Render from the template, possibly with updated body
-      rendered = email_template.render(assigns)
-
-      super(rendered.merge(headers.except(:body, :subject, 'body', 'subject')))
-    end
-
-    private
-
-    def route_url_assigns(email_template)
-      email_template.template_variables.select { |name| name.ends_with?('_url') }.inject({}) do |h, name|
-        h[name] = public_send(name) if respond_to?(name); h
-      end
-    end
-
+    include EffectiveEmailTemplatesMailer
   end
 end
