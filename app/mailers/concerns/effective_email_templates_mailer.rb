4e0bb20129c8c254e4822656dc950ed29c275f70
Matt Riemer <matthew@codeandeffect.com>
2023-09-25 12:18:32 -0600

    Dont assign route_urls when already assigned

--- app/mailers/concerns/effective_email_templates_mailer.rb
+++ app/mailers/concerns/effective_email_templates_mailer.rb
@@ -18,7 +18,7 @@ module EffectiveEmailTemplatesMailer
     email_template.subject = subject if subject.present?
 
     # Add any _url helpers
-    assigns = route_url_assigns(email_template).merge(assigns)
+    assigns = route_url_assigns(email_template, assigns).merge(assigns)
 
     # Render from the template, possibly with updated body
     rendered = email_template.render(assigns)
@@ -28,8 +28,12 @@ module EffectiveEmailTemplatesMailer
 
   private
 
-  def route_url_assigns(email_template)
-    email_template.template_variables.select { |name| name.ends_with?('_url') }.inject({}) do |h, name|
+  def route_url_assigns(email_template, existing)
+    route_variables = email_template.template_variables
+      .select { |name| name.ends_with?('_url') }
+      .reject { |name| existing.key?(name) || existing.key?(name.to_sym) }
+    
+    route_variables.inject({}) do |h, name|
       h[name] = public_send(name) if respond_to?(name); h
     end
   end
