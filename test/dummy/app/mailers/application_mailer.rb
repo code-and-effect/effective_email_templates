9641e68bec9f6b184d1a6aedea08845a4e959876
Matt Riemer <matthew@codeandeffect.com>
2023-10-26 10:34:25 -0600

    add test coverage

--- test/dummy/app/mailers/application_mailer.rb
+++ test/dummy/app/mailers/application_mailer.rb
@@ -4,17 +4,19 @@ class ApplicationMailer < ActionMailer::Base
 
   def welcome(resource, opts = {})
     @assigns = assigns_for(resource)
-    mail(to: resource.email)
+    mail(to: resource.email, **headers_for(resource, opts))
   end
 
   def assigns_for(user)
     raise('expected a User') unless user.kind_of?(User)
 
-    {
+    values = {
       email: user.email,
       first_name: user.first_name,
       last_name: user.last_name
     }
+
+    { user: values }
   end
 
 end
--- test/models/email_templates_test.rb
+++ test/models/email_templates_test.rb
@@ -6,14 +6,32 @@ class EmailTemplatesTest < ActiveSupport::TestCase
     assert email_template.valid?
   end
 
-  test 'welcome' do
+  test 'welcome with default content' do
+    email_template = build_effective_email_template()
+    email_template.save!
+
     user = build_user()
+    user.save!
 
     ApplicationMailer.welcome(user).deliver_now
     mail = ActionMailer::Base.deliveries.last
 
-    assert_equal "Welcome #{user.first_name}", mail.subject
+    assert_equal "Welcome #{user.first_name} #{user.last_name}", mail.subject
+    assert mail.body.to_s.start_with?("Welcome #{user.first_name} #{user.last_name}")
+  end
 
+  test 'welcome with overridden content' do
+    email_template = build_effective_email_template()
+    email_template.save!
+
+    user = build_user()
+    user.save!
+
+    ApplicationMailer.welcome(user, subject: "Hello #{user.first_name}", body: "Hey #{user.first_name}").deliver_now
+    mail = ActionMailer::Base.deliveries.last
+
+    assert_equal "Hello #{user.first_name}", mail.subject
+    assert mail.body.to_s.start_with?("Hey #{user.first_name}")
   end
 
 end
